9243761e83ba480949aba084cd9c0627a8757a48
Surya <49099412+suryapandiaraj@users.noreply.github.com>
2023-10-04 22:50:04 +0530

    Update DeluxePizza.java

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,12 @@
 package com.driver;
 
-public class DeluxePizza extends Pizza {
-
-    public DeluxePizza(Boolean isVeg) {
+public class DeluxePizza extends Pizza
+{
+    public DeluxePizza(Boolean isVeg)
+    {
         super(isVeg);
-        // your code goes here
+
+        addExtraCheese();
+        addExtraToppings();
     }
 }
